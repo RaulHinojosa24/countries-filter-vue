b42a1a9b2ec4e76ec40c7d47608bf308cfe90f1d
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-04-07 13:39:38 +0200

    Solution

--- main.js
+++ main.js
@@ -1,1 +1,42 @@
-// Suerte :)
+Vue.createApp({
+    data() {
+        return {
+            isDarkTheme: true,
+            countries: [],
+            selectFilter: "",
+            inputFilter: "",
+            modalCountry: ""
+        }
+    },
+    created() {
+        this.fetchData();
+    },
+    methods: {
+        async fetchData() {
+            this.countries = await fetch("https://restcountries.com/v2/all")
+                .then(response => response.json());
+        },
+        setModalCountry(code) {
+            console.log(code);
+            this.modalCountry = this.countries.filter(c => c.alpha3Code == code)[0];
+            console.log(this.modalCountry);
+        },
+        hideCountryModal() {
+            this.modalCountry = "";
+        }
+    },
+    computed: {
+        filteredCountries() {
+            const inputRegex = new RegExp(this.inputFilter, 'i');
+            const selectRegex = new RegExp(this.selectFilter, 'i');
+
+            return this.countries.filter(country => {
+                if (country.name.match(inputRegex) && country.region.match(selectRegex)) {
+                    return true;
+                }
+
+                return false;
+            });
+        }
+    },
+}).mount("#app");
